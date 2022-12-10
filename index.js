b7f087deed2ea97fb2cd3ccef90cc7196e5a58ff
sksurendrakmr <sksurendrakmr@gmail.com>
2022-12-11 02:16:42 +0530

    Cookie authentication

--- index.js
+++ index.js
@@ -7,42 +7,44 @@ const path = require('path');
 const Connection = require('./dbconfig')
 const Basic_Auth_Plugin = require('@hapi/basic');
 const BoomPlugin = require('@hapi/boom')
-//Mocked users
-const users = {
-    suri:{
-        username:"suri",
-        password:"sk",
-        id:0,
-        name:"sksuri"
-    },
-    sk:{
-        username:"sk",
-        password:"suri",
-        id:1,
-        name:"sksk"
-    },
-}
-const validate = async(request,username,password,h) => {
-    /**
-     * If validate returns "isValid:false" then the popup reappears
-     * and the page content is still hidden from the user. 
-     */
-    if(!users[username]){
-        return {isValid:false}
-    }
+const CookiePlugin = require("@hapi/cookie");
+const { request } = require('http');
+// //Mocked users
+// const users = {
+//     suri:{
+//         username:"suri",
+//         password:"sk",
+//         id:0,
+//         name:"sksuri"
+//     },
+//     sk:{
+//         username:"sk",
+//         password:"suri",
+//         id:1,
+//         name:"sksk"
+//     },
+// }
+// const validate = async(request,username,password,h) => {
+//     /**
+//      * If validate returns "isValid:false" then the popup reappears
+//      * and the page content is still hidden from the user. 
+//      */
+//     if(!users[username]){
+//         return {isValid:false}
+//     }
 
-    const user = users[username]; 
+//     const user = users[username]; 
 
-    /**
-     * The object passed to crednetials can be accessed in the handler by
-     * request.auth.credentials.
-     */
-    if(user.password === password){
-        return {isValid:true,credentials:{id:user.id,name:user.name}};
-    }else{
-        return {isValid:false}
-    }
-}
+//     /**
+//      * The object passed to crednetials can be accessed in the handler by
+//      * request.auth.credentials.
+//      */
+//     if(user.password === password){
+//         return {isValid:true,credentials:{id:user.id,name:user.name}};
+//     }else{
+//         return {isValid:false}
+//     }
+// }
 
 //Create server function
 //Wrap in async function so we can make use of await keyword.
@@ -69,15 +71,50 @@ const init = async() => {
     {
         plugin:Inert,
     },
+    // {
+    //     plugin:Basic_Auth_Plugin //1. Register Basic Auth plugin
+    // },
     {
-        plugin:Basic_Auth_Plugin //1. Register Basic Auth plugin
-    },
+        plugin:CookiePlugin
+    }
 ])
 
-//2. Implement Strategy
-server.auth.strategy('login','basic',{
-    validate
+// //2. Implement Strategy
+// server.auth.strategy('login','basic',{
+//     validate
+// })
+
+server.auth.strategy('login','cookie',{
+    cookie:{
+        name:'session',
+        password:'surisksurisksurisksurisksurisksurisk',
+        isSecure:true,
+        ttl: 30000
+    },
+    redirectTo: '/login',
+    //validate if cookie is still valid.
+    //session object that we create if user is successfully loggedIn.
+    validateFunc: async(request,session) => {
+        /**
+         * ValidateFunc
+         * ====
+         * - A function used to validate the content of the cookie on each request.
+         * - Must return an object with the key valid and value true(if authenticated) or false(if not authenticated)
+         * - Used to verify that the user session with the server is still valid.
+         * 
+         * - Signature -> function(request, session)
+         *   where request is the Hapi request object being authenticated and session is the object set with request.cookieAuth.set()
+         */
+
+        if(session.username === "sk" && session.password==="sksuri"){
+            return {valid:true, credentials:{favouriteFood:'pizza'}}
+        }else{
+            return {valid:false}
+        }
+    }
 })
+
+server.auth.default('login')
 
 // We can specify the strategy on select routes by using options=> auth => scheme name 
 //we can specify the strategy to work on every route using
@@ -106,11 +143,17 @@ server.auth.strategy('login','basic',{
         handler:(req,h)=>{
             //inert will provide a method called file in response toolkit to serve a file to client from server
             // return h.file('./staic/welcome.html');
+            if(req.auth.isAuthenticated()){
+                return h.redirect('/welcome')
+            }
             return h.file('welcome.html')
         },
         options:{
-            files:{
-                relativeTo:path.join(__dirname,'static')
+            // files:{
+            //     relativeTo:path.join(__dirname,'static')
+            // }
+            auth:{
+                mode:'try'
             }
         }
     })
@@ -163,6 +206,11 @@ server.auth.strategy('login','basic',{
         path:'/{any*}', //can be of any name
         handler:(req,h)=>{
             return `<h1>The page not found!!!<h1/>`;
+        },
+        options:{
+            auth:{
+                mode:'try'
+            }
         }
     })
 
@@ -172,12 +220,28 @@ server.auth.strategy('login','basic',{
         path:'/login',
         handler:(req,h)=>{
             const {username,password} = req.payload;
-            if(username==="suri" && password==="1234"){
-                // return `<h1>You logged In<h1/>`
-                return h.file('LoggedIn.html');
+            if(username==="sk" && password === "sksuri"){
+                //send back the cookie to user that contain login information.
+                //The informations which we set here, can be access via session object which is 2nd parameter of validateFunc
+                request.cookieAuth.set({username:username,password:password})
+                return h.redirect('./welcome.html')
             }else{
-                return h.redirect('/');
+                return h.redirect('/login')
             }
+        },
+        options:{
+            auth:{
+                mode:'try'
+            }
+        }
+    })
+
+    server.route({
+        method:'GET',
+        path:'/welcome',
+        handler:(request,h)=>{
+            // return h.file('welcome.html')
+            return `Welcome ${request.auth.credentials.username}` //which we have set in cookieAuth
         }
     })
 
@@ -209,7 +273,9 @@ server.auth.strategy('login','basic',{
         method:'GET',
         path:'/logout',
         handler:(request,h) => {
-            return BoomPlugin.unauthorized("You have been logged out successfully");
+            request.cookieAuth.clear();
+            return h.redirect('/')
+            // return BoomPlugin.unauthorized("You have been logged out successfully");
         }
     })
 
