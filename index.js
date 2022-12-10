b7e744ac8fce39cfb2731eb6551542bfdaaaf9b5
sksurendrakmr <sksurendrakmr@gmail.com>
2022-12-10 15:08:25 +0530

    Intro to Hapi - config of server and starting the server

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 'use-strict'
 
+const Hapi = require('@hapi/hapi');
+
+//Create server function
+//Wrap in async function so we can make use of await keyword.
+const init = async() => {
+
+    const server = Hapi.server({
+        host:'localhost',
+        port:5050
+    })
+
+    server.route({
+        method:'GET',
+        path:'/',
+        handler:(req,h)=>{
+            return "Hello Hapi";
+        }
+    })
+
+    //start server
+    await server.start();
+    console.log(`Server started on: ${server.info.uri}`);
+}
+
+process.on('unhandledRejection',(err)=>{
+    console.log(err);
+    process.exit(1);
+})
+
+init();
